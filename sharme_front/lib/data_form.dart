be749db303342298fcb407bf9c605e33f4bceb00
kevin <kevin-hamza.djeradi@epitech.eu>
2024-04-16 18:28:09 +0200

    added app icon

--- sharme_front/lib/data_form.dart
+++ sharme_front/lib/data_form.dart
@@ -1,7 +1,5 @@
 import 'package:contacts_service/contacts_service.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
